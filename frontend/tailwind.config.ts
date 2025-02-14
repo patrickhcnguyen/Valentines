6d248982f79a9b6f0cd38cd7af629fa88cfcef01
patrickhcnguyen <patricknguyen714@gmail.com>
2025-02-13 19:17:28 -0800

    config stuff

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -12,6 +12,10 @@ export default {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
+      fontFamily: {
+        amiri: ['Amiri', 'Arial', 'sans-serif'],
+        mooMoo: ['SwankyandMooMoo', 'Arial', 'sans-serif'],
+      }
     },
   },
   plugins: [],
